c63aea8f00482dd01bb01e8a65200041c8de1893
Kaji0tt <Hydro-Jascha@web.de>
2020-03-26 07:16:17 +0100

    Unser Little Grinder Projekt
    The whole Unity Project in here.

---
+++ Assets/Scripts/IsometricPlayerMovementController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerMovementController : MonoBehaviour
+{
+
+    public float movementSpeed = 1f;
+    IsometricCharacterRenderer isoRenderer;
+
+    Rigidbody2D rbody;
+    //BoxCollider2D rbody;
+
+    Vector3 forward, right;
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        //rbody = GetComponent<BoxCollider2D>();
+        isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+
+        // **MOVE SPLASH** // Hier Splash ich das Movement aus dem anderen Tutorial in den Code mit dem 2D Movement Animator
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // ** MOVE SPLASH ** //
+        if (Input.anyKey)
+            Move();
+
+        //Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("HorizontalKey");
+        float verticalInput = Input.GetAxis("VerticalKey");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        //inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        //Vector2 movement = inputVector * movementSpeed;
+        //Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        isoRenderer.SetDirection(inputVector);
+        //rbody.MovePosition(newPos);
+    }
+        // ** MOVE SPLASH METHODE ** //
+    void Move()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        Vector3 rightMovement = right * movementSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        Vector3 upMovement = forward * movementSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
+
+        //Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+
+        //transform.forward = heading;
+        /* Transform.Position ignoriert die Physic und deshalb sämltliche Colider 
+         * Nicht verwenden, wenn's anders geht!!*/
+        //transform.Translate(rightMovement * movementSpeed);
+        //transform.Translate(upMovement * movementSpeed);
+        transform.position += rightMovement;
+        transform.position += upMovement;
+        
+
+    }
+
+}
+
