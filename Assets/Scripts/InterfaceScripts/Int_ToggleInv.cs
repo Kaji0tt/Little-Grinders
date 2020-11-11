896ec4db79f89d06287297ea32d8bb3e46f06bd4
Kaji0tt <Hydro-Jascha@web.de>
2020-11-11 02:00:53 +0100

    Added SkillTree
    Added SkillTree, SkillTree Mechanics, Basic UI für Skills

--- Assets/Scripts/InterfaceScripts/Int_ToggleInv.cs
+++ Assets/Scripts/InterfaceScripts/Int_ToggleInv.cs
@@ -6,43 +6,59 @@ using UnityEngine.UI;
 public class Int_ToggleInv : MonoBehaviour
 {
 
-    public GameObject CharacterMenu, InventoryTab, SkillTab;
+    public GameObject characterMenu, inventoryTab, skillTab;
 
     [Space]
     //public GameObject[] CharakterMenus; //int[] array2 = new int[] { 1, 3, 5, 7, 9 }
-    private bool ToggleInv;
+    private bool toggleInv;
     private Image[] InventoryItems;
+
+    private int currentMenu;
 
     private void Start()
     {
         //InventoryTab = Get
-        ToggleInv = false;
+        toggleInv = false;
     }
 
     // Update is called once per frame
     void Update()
     {
      //   print(ToggleInv);
-        if (Input.GetKeyDown(KeyCode.E) && ToggleInv==false)
+        if (Input.GetKeyDown(KeyCode.E) && toggleInv==false)
         {
-            ToggleInv = true;
-            CharacterMenu.SetActive(true);
+            toggleInv = true;
+            characterMenu.SetActive(true);
+            currentMenu = 1;
         }
 
-        else if(Input.GetKeyDown(KeyCode.E) && ToggleInv==true)
+        else if(Input.GetKeyDown(KeyCode.E) && toggleInv==true)
         {
-            ToggleInv = false;
-            CharacterMenu.SetActive(false);
+            toggleInv = false;
+            characterMenu.SetActive(false);
         }
 
-        if (ToggleInv == true)
+        if (toggleInv == true)
         {
-            if (Input.GetKey(KeyCode.Tab))          //Ekliger Kladaradatsch. Eigentlich aus einem Array ausgelesen, die einzelnen Objekte durchschalten.
+            if (Input.GetKeyDown(KeyCode.Tab))          //Ekliger Kladaradatsch. Eigentlich aus einem Array ausgelesen, die einzelnen Objekte durchschalten.
             {
 
+
+                switch (currentMenu)
+                {
+                    case 1:
+                        inventoryTab.SetActive(false);
+                        skillTab.SetActive(true);
+                        currentMenu = 2;
+                        break;
+                    case 2:
+                        inventoryTab.SetActive(true);
+                        skillTab.SetActive(false);
+                        currentMenu = 1;
+                        break;
+                }
                 //if InventoryTab.SetActive(true);
-                InventoryTab.SetActive(false);
-                SkillTab.SetActive(true);
+
                 
                 
             }
