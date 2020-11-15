4fc61fdfa5038482d9d4e17c5309ef1ca4af4cd4
Kaji0tt <Hydro-Jascha@web.de>
2020-11-15 03:10:36 +0100

    Configured new AnimationClass
    Aber irgendwie spinnt die noch..

---
+++ Assets/Scripts/Enemy/EnemyAnimator.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private GameObject Front, Back;
+
+    private void Awake()
+    {
+        animator.GetComponent<Animator>();
+    }
+
+
+    public void AnimateMe(Vector2 inputVector, float player_distance, float attackRange, float aggroRange)
+    {
+        animator.SetFloat("AnimDistance", player_distance);
+
+        if (inputVector.y <= 0)
+        {
+            AnimateFront(player_distance, attackRange, aggroRange);
+        }
+            
+
+        if (inputVector.y >= 0)
+        {
+
+            AnimateBack(player_distance, attackRange, aggroRange);
+        }
+
+        /*
+        if (inputVector.x <= 0)
+            AnimateSide_Left(player_distance, attackRange, aggroRange);
+
+        if (inputVector.x >= 0)
+            AnimateSide_Right(player_distance, attackRange, aggroRange);
+            */
+
+
+    }
+
+    private void AnimateSide_Right(float player_distance, float attackRange, float aggroRange)
+    {
+        animator.SetFloat("AnimDistance", player_distance);
+
+        if (animator.GetFloat("AnimDistance") <= attackRange)
+        {
+            animator.Play("R_Attacking");
+        }
+        else if (animator.GetFloat("AnimDistance") <= aggroRange)
+        {
+            animator.Play("R_Chasing");
+        }
+        else if (animator.GetFloat("AnimDistance") >= aggroRange)
+        {
+            animator.Play("R_Idle");
+        }
+    }
+
+    private void AnimateSide_Left(float player_distance, float attackRange, float aggroRange)
+    {
+        animator.SetFloat("AnimDistance", player_distance);
+
+        if (animator.GetFloat("AnimDistance") <= attackRange)
+        {
+            animator.Play("L_Attacking");
+        }
+        else if (animator.GetFloat("AnimDistance") <= aggroRange)
+        {
+            animator.Play("L_Chasing");
+        }
+        else if (animator.GetFloat("AnimDistance") >= aggroRange)
+        {
+            animator.Play("L_Idle");
+        }
+    }
+
+    private void AnimateFront(float player_distance, float attackRange, float aggroRange)
+    {
+        Front.SetActive(true);
+        Back.SetActive(false);
+
+
+            //animator.SetFloat("AnimDistance", player_distance);
+
+
+            if (animator.GetFloat("AnimDistance") <= attackRange)
+            {
+                animator.Play("F_Attacking");
+            }
+            else if (animator.GetFloat("AnimDistance") <= aggroRange)
+            {
+                animator.Play("F_Chasing");
+            }
+            else if (animator.GetFloat("AnimDistance") >= aggroRange)
+            {
+                animator.Play("F_Idle");
+            }
+        
+    }
+
+    private void AnimateBack(float player_distance, float attackRange, float aggroRange)
+    {
+        Back.SetActive(true);
+        Front.SetActive(false);
+
+        print(player_distance);
+        //animator.SetFloat("AnimDistance", player_distance);
+        if (animator.GetFloat("AnimDistance") <= attackRange)
+        {
+            animator.Play("B_Attacking");
+        }
+        else if (animator.GetFloat("AnimDistance") <= aggroRange)
+        {
+            animator.Play("B_Chasing");
+        }
+        else if (animator.GetFloat("AnimDistance") >= aggroRange)
+        {
+            animator.Play("B_Idle");
+        }
+    }
+        
+}
---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+public class EnemyController : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform character_transform;
+
+    NavMeshAgent navMeshAgent;
+    //IsometricCharacterRenderer isoRenderer;
+    //private Animator animator;
+    private EnemyAnimator enemyAnimator;
+
+    ///----Combat Variables-----
+    ///
+    CharacterCombat combat;
+    IsometricPlayer isometricPlayer;
+    private GameObject scriptController;
+    ItemDatabase itemDatabase;
+    public GameObject hpBar;
+    public Slider enemyHpSlider;
+
+
+    ///-----Stat Stuff-----
+    [Header("Stats")]
+    private float attackCD = 0f;
+    public CharStats Hp, Armor, AttackPower, AbilityPower, AttackSpeed;
+    private float maxHp;
+    [Space]
+    public int Experience;
+    public float aggroRange = 5f, attackRange;
+    //private PlayerStats playerStats;
+    public LootTable lootTable;
+
+
+    ///----Position/Ziel/Direction----
+    Vector3 forward, right;
+    private float player_distance;
+    private Vector3 targetVector;
+    Vector2 inputVector;
+
+
+    void Start()
+    {
+        combat = GetComponent<CharacterCombat>();
+        scriptController = GameObject.FindWithTag("GameController");
+        itemDatabase = scriptController.GetComponent<ItemDatabase>();
+        enemyAnimator = GetComponentInChildren<EnemyAnimator>();
+        //animator = GetComponent<Animator>();
+
+
+        maxHp = Hp.Value;
+
+
+
+        // Spätestens wenn ich mehrere Level habe und der Spawn vom Player neu gesetzt wird, wird ein durchgehender Singleton nötig sein.
+        character_transform = PlayerManager.instance.player.transform;
+        //PlayerStats playerStats = destination.GetComponent<PlayerStats>();
+
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+        //isoRenderer = GetComponentInChildren<IsometricCharacterRenderer>();
+    }
+
+
+    void Update()
+    {
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        player_distance = Vector3.Distance(character_transform.position, transform.position);
+
+        enemyAnimator.AnimateMe(inputVector, player_distance, attackRange, aggroRange);
+
+        if (navMeshAgent == null)
+        {
+            Debug.LogError("The Nav Mesh agent Component is not attached to " + gameObject.name);
+        }
+
+        else if (player_distance <= aggroRange)
+        {
+            SetDestination();           
+
+            if (player_distance <= attackRange)
+            {
+
+                PlayerStats playerStats = character_transform.GetComponent<PlayerStats>();
+                attackCD -= Time.deltaTime;
+                if (playerStats != null)
+                {
+                    if (attackCD <= 0)
+                    {
+                        playerStats.TakeDamage(AttackPower.Value);
+                        attackCD = 1f / AttackSpeed.Value;
+                    }
+                }
+            }
+        }
+
+        else
+            navMeshAgent.SetDestination(transform.position);
+
+
+        #region Hp-Bar
+
+        enemyHpSlider.value = Hp.Value / maxHp;
+        if (Hp.Value < maxHp)
+            hpBar.SetActive(true);
+
+        if (Hp.Value <= 0)
+            Destroy(gameObject);
+        #endregion
+
+    }
+
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.name == "DirectionCollider") // Andere Lösung finden zur Liebe des CPU. // Singleton für DirectionCollider?
+        {
+            PlayerStats playerStats = character_transform.GetComponent<PlayerStats>();
+            playerStats.attackCD -= Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (playerStats.attackCD <= 0)
+                {
+                    TakeDamage(playerStats.AttackPower.Value, playerStats.Range);
+                    playerStats.attackCD = 1f / playerStats.AttackSpeed.Value;
+                }
+            }
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (character_transform != null)
+        {
+
+            navMeshAgent.SetDestination(character_transform.transform.position);
+
+            // Hier wird die "Blickrichtung" bestimmt, welche sich an dem Charakter orientiert.
+            Vector3 Direction = character_transform.transform.position - transform.position;
+            Vector2 inputVector = new Vector2(Direction.x * -1, Direction.z);
+            inputVector = Vector2.ClampMagnitude(inputVector, 1);
+            //isoRenderer.SetNPCDirection(inputVector);
+
+
+            //irgendwo hier ist noch n kleiner fehler
+
+
+        }
+    }
+
+    public void TakeDamage(float damage, int range)
+    {
+        player_distance = Vector3.Distance(character_transform.position, transform.position);
+        if (player_distance <= range)
+        {
+            damage = 10 * (damage * damage) / (Armor.Value + (10 * damage));            // DMG & Armor als werte
+            damage = Mathf.Clamp(damage, 1, int.MaxValue);
+            Hp.AddModifier(new StatModifier(-damage, StatModType.Flat));
+
+
+        }
+
+        if (Hp.Value <= 0)
+            Die();
+    }
+
+
+    public void Die()
+    {
+        //print("enemy died");
+        PlayerStats playerStats = character_transform.GetComponent<PlayerStats>();
+        playerStats.Set_xp(Experience);
+
+        string _lootTable = lootTable.ToString();
+        itemDatabase.GetComponent<ItemDatabase>().GetDrop(gameObject.transform.position, _lootTable);
+        Destroy(gameObject);
+    }
+
+    /*
+    private void AnimateMe()
+    {
+        animator.SetFloat("AnimDistance", player_distance);
+        
+
+        if(animator.GetFloat("AnimDistance") <= attackRange)
+        {
+            animator.Play("F_Attacking");
+        }
+        else if (animator.GetFloat("AnimDistance") <= aggroRange)
+        {
+            animator.Play("F_Chasing");
+        }
+        else if (animator.GetFloat("AnimDistance") >= aggroRange)
+        {
+            animator.Play("F_Idle");
+        }
+    }
+    */
+
+}
